dc84434f132e29d20e60d33f5291b2d5c8350497
vikoms <vikoms95@gmail.com>
2019-08-30 20:50:25 +0700

    Menambah Data

--- app/src/main/java/app/android/mymail/MainActivity.java
+++ app/src/main/java/app/android/mymail/MainActivity.java
@@ -34,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         listMail.add(new Mail("Rudi","Belajar Bersama" , "Hallo Ayo Kita Belajar " , "11:20 AM" , R.drawable.ic_school_black_24dp));
         listMail.add(new Mail("Google","Lowongan Kerja" , "Bekerja di Google Yuk" , "08:45 AM" , R.drawable.ic_assignment_ind_black_24dp));
         listMail.add(new Mail("Hire.Co","Informasi" , "Lihat Internet Lebih Luas" , "12:00 AM" , R.drawable.ic_public_black_24dp));
+        listMail.add(new Mail("Jordan","Liburan Bersama","Mari kita liburan ke Bali","04:32pm",R.drawable.ic_beach_access_black_24dp));
+        listMail.add(new Mail("Samsul","Latihan Futsal","Latihan futsal hari ini","01.00pm",R.drawable.ic_person_outline_black_24dp));
+        listMail.add(new Mail("Budiman","Berfoto Bersama","Mari kita berfoto di Taman","06:02am",R.drawable.ic_photo_camera_black_24dp));
+        listMail.add(new Mail("Gatot","Berenang Bersama","Mari berenang di kali","04:50pm",R.drawable.ic_pool_black_24dp));
 
     }
 }
